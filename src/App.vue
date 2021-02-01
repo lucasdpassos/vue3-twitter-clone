1bcf364a2ed0d4ebe48bfe75b1254bf69b5e0e1c
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-02-01 17:34:56 -0300

    user profile

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,36 @@
 <template>
-  <div>
-    aaaaa
+  <div id="app">
+    <UserProfile />
   </div>
 </template>
 
+
+
 <script>
+
+import UserProfile from './components/UserProfile'
 
 
 export default {
   name: 'App',
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id:1,
-        username:"LucasPassos",
-        firstName:"Lucas",
-        
-      }  
-    }
-  },
   components: {
-   //
-  }
+    UserProfile
+    
 }
+}
+
+
+
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  -webkit-font-smoothing: antialiased;  
   color: #2c3e50;
-  margin-top: 60px;
+  
+  
+
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile_user-panel">
+        <h1 class="user-profile_username">
+             @{{ user.username }}
+        </h1>
+        <div class="user-profile_admin-badge" v-if="user.isAdmin">
+            admin
+        </div>
+    <div class="user-profile_follower-count">
+ <strong>Followers: </strong> {{ followers }}
+    </div>  
+</div>
+<div class="user-profile_tweets-wrapper">
+    
+</div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'UserProfile',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id:1,
+        username:"LucasPassos",
+        firstName:"Lucas",
+        lastName:"Passos",
+        email:"lucas.depassos@yahoo.com.br",
+        isAdmin:true,
+        tweets:[
+            { id:1, content:"Nice!"}
+        ]
+        
+      }  
+    }
+  },
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if(oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName} ` 
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser()
+  }
+}
+
+
+
+</script>
+
+<style>
+.user-profile {
+    display:grid;
+    grid-template-columns:1fr 3fr;
+    width:100%;
+    padding:50px 5%;
+
+}
+
+.user-profile_user-panel {
+    display:flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding:20px;
+    background-color: white;
+    border-radius:5px;
+    border:1px solid #DFE3E8;
+}
+
+h1 {
+    margin: 0;
+}
+.user-profile_admin-badge {
+    background: rebeccapurple;
+    color: white;
+    border-radius: 5px;
+    margin-right:auto;
+    padding: 0 10px;
+    font-weight: bold;
+
+}
+
+
+</style>
