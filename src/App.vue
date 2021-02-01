2133fbcd43bd1030e4dd3007e2af3552bd98771e
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-02-01 14:45:34 -0300

    first_commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    aaaaa
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id:1,
+        username:"LucasPassos",
+        firstName:"Lucas",
+        
+      }  
+    }
+  },
+  components: {
+   //
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
